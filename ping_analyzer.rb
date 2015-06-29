4ca721842c5b90e1f172983adb5ac72c5e9605e0
barrywilliams <bewilliams@williamsgarcia.com>
2015-06-29 01:13:51 -0400

    Hopefully this fixes an issue with ping on my pi

--- ping_analyzer.rb
+++ ping_analyzer.rb
@@ -1,13 +1,13 @@
 def analyze_ping(target, raw_ping, raw_error)
-  raw_line = raw_ping[1].strip
 
   #empty string if no internet
-  if raw_line.strip.length == 0
+  if raw_ping.empty? || raw_ping[1].nil? || raw_ping[1].strip.length == 0
     time = nil
     ttl = nil
     bytes = nil
     connected = false
   else
+    raw_line = raw_ping[1].strip
     time = raw_line[/(?<=time=)\d*.?\d*/].to_f
     ttl = raw_line[/(?<=ttl=)\d*/].to_i
     bytes = raw_line[/^\d*/].to_i
