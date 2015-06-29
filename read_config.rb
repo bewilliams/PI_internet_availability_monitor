b5c5a0f48944d7aafc77412813ead3a97987fdfc
barrywilliams <bewilliams@williamsgarcia.com>
2015-06-29 00:31:53 -0400

    Fixed a bug in the config reader

--- read_config.rb
+++ read_config.rb
@@ -2,8 +2,8 @@ def read_config
   raw_config = File.read('./config/ping_servers.conf')
   config = []
   raw_config.split("\n").each do |line|
-    name, address = line.split(":").collect {|x| x.strip!}
-    config << {name: name, address: address}
+    name, address = line.split(":")
+    config << {name: name.strip, address: address.strip}
   end
   config
 end
